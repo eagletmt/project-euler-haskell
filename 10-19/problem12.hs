6dc90899b1102c2f0ed4c232b86e358bd928a962
eagletmt <eagletmt@gmail.com>
2009-05-28 01:03:28 +0900

    add .gitignore fix problem12.hs: use scanl1 instead of mapAccumL

--- 10-19/problem12.hs
+++ 10-19/problem12.hs
@@ -5,7 +5,7 @@ divisors :: (Integral a) => a -> [a]
 divisors n = concat [x | i <- [1..floor (sqrt (fromIntegral n))], let (q,r) = n `divMod` i, r == 0, let x = if i == q then [i] else [i,q]]
 
 triangles :: (Integral a) => [a]
-triangles = snd $ mapAccumL (\a x -> (a+x, a)) 1 [2..]
+triangles = scanl1 (+) [1..]
 
 main = print $ fromJust $ find (\i -> length (divisors i) > 500) $ triangles
 
