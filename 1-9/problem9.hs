01f451acea5fe1b4f58ed646eaca63919e087919
eagletmt <eagletmt@gmail.com>
2009-05-27 23:10:55 +0900

    fix problem9.hs

--- 1-9/problem9.hs
+++ 1-9/problem9.hs
@@ -1,4 +1,2 @@
-main = print $ uncurry3 (*) $ head [(a,b,1000-a-b) | a <- [1..333], b <- [a..666], let c = 1000-a-b in a*a + b*b == c*c]
-  where
-    uncurry3 :: (a -> a -> a) -> (a,a,a) -> a
-    uncurry3 f (a,b,c) = f a $ f b c
+main = print $ head [a*b*c | a <- [1..333], b <- [a..666], let c = 1000-a-b, a*a + b*b == c*c]
+
